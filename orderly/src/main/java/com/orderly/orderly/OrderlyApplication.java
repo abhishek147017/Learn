a5b8edcef3dffd240ef916fcf1af2448eb4d87d1
root <root@localhost.localdomain>
2026-01-22 08:30:01 +0000

    added sh file to install all dependancies

--- orderly/src/main/java/com/orderly/orderly/OrderlyApplication.java
+++ orderly/src/main/java/com/orderly/orderly/OrderlyApplication.java
@@ -7,20 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class OrderlyApplication {
 
     public static void main(String[] args) {
-//    hello this is OrderlyApplication
-//        com.orderly.orderly
-//├── OrderlyApplication.java
-//├── controller
-//├── service
-//│ └── impl
-//├── repository
-//├── entity
-//├── dto
-//├── exception
-//├── config
-//└── util
-        SpringApplication.run(OrderlyApplication.class, args
-        );
+        SpringApplication.run(OrderlyApplication.class, args);
     }
 
 }
